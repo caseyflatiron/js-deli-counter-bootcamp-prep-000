7a1a858ccf6f36e4d5898f8c1e85d5b821cc67b7
casey bradford <casey@flatironschool.com>
2020-04-19 22:10:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(lineArray, name){
   lineArray.push(name);
-  return "Welcome, ${name}. You are ${lineArray.length} in line."
+  return `Welcome, ${name}. You are ${lineArray.length + 1} in line.`
 }
